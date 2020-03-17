cd4ccbca81158d18064fa2481c0a985b5ee3e2bd
KeirDrummond <k.drummond2@uni.brighton.ac.uk>
2020-03-17 01:01:15 +0000

    Made to inherit the Phaser Game Object class, implemented colour and an on disconnect function.

--- client/js/player.js
+++ client/js/player.js
@@ -1,11 +1,20 @@
-class Player {
-    constructor(id, x, y) {
+class Player extends Phaser.GameObjects.GameObject {
+    constructor(id, x, y, colour) {
+        super(game, 'Player');
+        
         this.id = id;
         this.x = x;
         this.y = y;
+        this.colour = colour;
         
         this.sprite = game.add.sprite(x, y, 'sprite');
+        this.sprite.tint = this.colour;
         console.log("New player added");
+    }
+    
+    OnDisconnect() {
+        this.sprite.destroy();
+        this.destroy();
     }
     
     update() {
